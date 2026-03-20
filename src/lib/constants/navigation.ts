2a3e626a3df3d15f519a29a32c7c9373850a992d
jsg3121 <xodm95@gmail.com>
2026-03-20 13:44:09 +0900

    🎨 feat : 사이드 프로젝트 페이지 퍼블리싱 및 목업 데이터 추가

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -4,7 +4,7 @@ interface NavItem {
 }
 
 export const NAV_ITEMS: NavItem[] = [
+  { label: 'About', href: '/about' },
   { label: 'Work', href: '/work' },
   { label: 'Side Projects', href: '/side-projects' },
-  { label: 'About', href: '/about' },
 ];
