e88b87b4689db7495d2acf8707321d35a9d7286f
jsg3121 <xodm95@gmail.com>
2026-03-30 18:00:22 +0900

    🛠️ feat : rehype 플러그인 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://sungyujang.com',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+        },
+      ],
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
